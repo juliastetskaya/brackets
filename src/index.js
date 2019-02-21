bf1399d7c3622f3252da5a79f118eefae35a2ef0
juliastetskaya <juliastetskaya@gmail.com>
2019-02-21 12:43:42 +0300

    Ready!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = new Map(bracketsConfig);
+  const isOpeningSymbol = symbol => brackets.has(symbol);
+  const getClosingSymbol = symbol => brackets.get(symbol);
+  const isStackEmpty = stack => (stack.length === 0);
+  const stack = [];
+  for (const s of str) {
+    if (isOpeningSymbol(s)) {
+      const closingSymbol = getClosingSymbol(s);
+      stack.push(closingSymbol);
+      if (s === closingSymbol) {
+        const len = stack.length;
+        if (stack[len - 1] === stack[len - 2]) {
+          stack.splice(len - 2, 2);
+        }
+      }
+    } else {
+      if (isStackEmpty(stack)) {
+        return false;
+      }
+      const last = stack.pop();
+      if (last !== s) {
+        return false;
+      }
+    }
+  }
+
+  return isStackEmpty(stack);
+};
